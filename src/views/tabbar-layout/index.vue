7ebb64f930ee7756ce61ec406abfa83dd24318d6
laozhao <z1727080357@163.com>
2019-07-12 10:17:29 +0800

    完成路由缓存

--- src/views/tabbar-layout/index.vue
+++ src/views/tabbar-layout/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <router-view></router-view>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
     <van-tabbar route>
       <van-tabbar-item icon="home-o"
                        to="/">首页</van-tabbar-item>
